562c6e0596be58a6be43c78f36e7b35239ce9e64
Suyog Dharmadhikari <sdharma2@ncsu.edu>
2022-12-09 16:01:41 -0500

    created bankData Frame

--- server.R
+++ server.R
@@ -15,20 +15,10 @@ bankData <- read_csv("data/bankData.csv")
 
 
 
-# # Define server logic required to draw a histogram
-# shinyServer(function(input, output) {
-# 
-#     output$distPlot <- renderPlot({
-# 
-#         # generate bins based on input$bins from ui.R
-#         x    <- faithful[, 2]
-#         bins <- seq(min(x), max(x), length.out = input$bins + 1)
-# 
-#         # draw the histogram with the specified number of bins
-#         hist(x, breaks = bins, col = 'darkgray', border = 'white',
-#              xlab = 'Waiting time to next eruption (in mins)',
-#              main = 'Histogram of waiting times')
-# 
-#     })
-# 
-# })
+# Define server logic required to draw a histogram
+shinyServer(function(input, output) {
+
+  
+    observe({bankData})
+
+})

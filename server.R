3d2e124754c6cea8807fac2cb67f30ed80e27f35
Suyog Dharmadhikari <sdharma2@ncsu.edu>
2022-12-11 02:55:48 -0500

    added code for about, data and data exploration page.

--- server.R
+++ server.R
@@ -9,16 +9,132 @@
 
 library(shiny)
 library(tidyverse)
+library(DT)
+library(dplyr)
+library(ggpubr)
 
 
-bankData <- read_csv("data/bankData.csv")
 
 
 
-# Define server logic required to draw a histogram
-shinyServer(function(input, output) {
-
+shinyServer(function(input, output, session) {
   
-    observe({bankData})
+  #Reading the dataset
+  bankDataUpdt <- reactive({
+    
+    bankData <- read_csv("data/bankData.csv")
+    bankData <- bankData %>%
+      mutate(education = gsub("\\.", " ",education)) %>%
+      mutate(pdays = if_else(pdays == 999,0, pdays))
+    bankData <- mutate_if(bankData, is.character, as.factor)
+    return(bankData)
+    
+  })
+  
+  
+  #Data frame with updated col names
+  updtBankData <- reactive({
+    
+    updtBankData <- bankDataUpdt()
+    colnames(updtBankData) <- c("Age","Job Type", "Marital status", "Education", "Has Credit", "Housing", "Loan", "Contact Type",
+                                "Month", "Day_of_the_week","Last_contact_duration","Campaign", "pDays(Last contacted)","Previous(Previously contacted or not)",
+                                "pOutcome(previous marketing campaign)", "Term deposit")
+    updtBankData <- mutate_if(updtBankData, is.character, as.factor)
+    
+    return(updtBankData)
+    
+  })
+  
+#--------------------------------------------------------------------------------------------------------------------------#
+#---------------------Data Page-------------------------------------------------------------------------------------------#
+#-------------------------------------------------------------------------------------------------------------------------# 
+  
+  #Subsetting the data based on the values of the input
+  theBankData <- reactive(updtBankData() %>%
+                            select(all_of(input$selectVariables)) %>%
+                            slice(1:input$numberOfRows))
+  
+  output$table <- renderDataTable({
+    
+    theBankData()
+    
+  },options = list(scrollX = TRUE, scrollY = TRUE, fixedHeader = TRUE))
+  
+  #Functionality to download the subsetted data
+  output$download <- downloadHandler(
+    filename = function(){"BankData.csv"}, 
+    content = function(fname){
+      write.csv(theBankData(), fname)
+    }
+  )
+  
 
+#-----------------------------------------------------------------------------------------------------------------------#
+#---------------------Plots--------------------------------------------------------------------------------------------#
+#----------------------------------------------------------------------------------------------------------------------# 
+  
+  #Handling bar plots
+  output$barPlot <- renderPlot({
+    
+    ggplot(bankDataUpdt() , aes_string(x = input$barPlotRadio)) +
+      geom_bar(aes(fill = y)) + 
+      scale_fill_discrete(name = "Type of Term deposit") + 
+      xlab(paste("Type of ",input$barPlotRadio) ) +
+      theme(axis.text.x = element_text(angle = input$twistAngle)) +
+      ggtitle(paste("Bar plot for", input$barPlotRadio))
+    
+  })
+  
+  #Handling Histogram plot
+  output$histPlot <- renderPlot({
+    
+    ggplot(data = bankDataUpdt(), aes_string(x = input$histPlotRadio)) + 
+      geom_histogram(aes(y = after_stat(density)),binwidth = input$binWidth, colour="black", fill="white") +
+      geom_density(alpha=.2, fill="#FF6666") +
+      scale_fill_discrete(name = "Type of Term deposit") +
+      xlab(input$histPlotRadio) + ggtitle(paste("Histogram for", input$histPlotRadio))
+    
+  })
+  
+#-----------------------------------------------------------------------------------------------------------------------#
+#---------------------Summaries----------------------------------------------------------------------------------------#
+#----------------------------------------------------------------------------------------------------------------------# 
+  
+  #subsetting the data and creating the summary dataTable
+  summaryData <- reactive({
+    
+    summaryDf <- bankDataUpdt() %>%
+      select(input$selectVariablesSumm) 
+    summDf <- apply(summaryDf, MARGIN = 2, FUN = summary)
+    rounded_summary <- round(summDf, input$roundValSumm)
+    summaryDatFrame <- data.frame(rounded_summary)
+    return(summaryDatFrame)
+    
+    
+  })
+  
+  #Displaying numerical summaries of the data
+  output$summTable <- renderDataTable({
+    
+    summaryData()
+    
+  })
+  
+  #Download functionality for summary table
+  output$downloadSumm <- downloadHandler(
+    filename = function(){"BankSummaryData.csv"}, 
+    content = function(fname){
+      write.csv(summaryData(), fname)
+    }
+  )
+  
+  #Contingency Table
+  
+  output$contTable <- renderDataTable({
+    var <- input$contingencyRadio
+    #print(get(var))
+    as.data.frame.matrix(table(bankDataUpdt()[var][[1]],bankDataUpdt()['y'][[1]]))
+    
+  })
+  
 })
--- ui.R
+++ ui.R
@@ -19,20 +19,92 @@ library(plotly)
 library(shinythemes)
 library(DT)
 library(rsconnect)
+library(jpeg)
+library(shinydashboard)
+library(dplyr)
+library(ggpubr)
 # ------------------
 # Main title section
 # ------------------
 
+colNameList <- c("Age","Job Type", "Marital status", "Education", "Has Credit", "Housing", "Loan", "Contact Type",
+                 "Month", "Day_of_the_week","Last_contact_duration","Campaign", "pDays(Last contacted)","Previous(Previously contacted or not)",
+                 "pOutcome(previous marketing campaign)", "Term deposit")
+
 ui <- navbarPage(
   "Bank Data Analysis",
-  theme = shinytheme("flatly"),
+  theme = shinytheme("cerulean"),
   tabPanel(
     "About",
-    # App title ----
     titlePanel(div(
-      windowTitle = "BankDataImage",
-      img(src = "Image/bankDateImage.jpg", width = "100%", class = "bg"),
+      windowTitle = "bankImageSG",
+      img(src = "bankImage2.jpg", width = "100%", class = "bg"),
     )),
+    tags$hr(),
+    tabsetPanel(
+      type = "tabs",
+      tabPanel( "Purpose of the App", 
+                tags$style(HTML(
+                  "#my-box {
+                  font-family: 'Times New Roman';
+                        font-size: 16px;}"
+                )),
+                box(title = "",
+                    id = "my-box",
+                    width = 100,
+                    height = 300,
+                   p("The Portuguese Bank has recently experienced a decline in revenue, and they are looking for ways to reverse this trend. 
+                     After investigating the issue, they discovered that their customers are not investing enough in long-term deposits, which 
+                     is negatively impacting the bank's performance. As a result, the bank is interested in identifying which existing customers 
+                     are more likely to invest in long-term deposits, and directing their marketing efforts towards these customers. The app is 
+                     designed to help users identify these potential customers and focus their marketing efforts accordingly. I believe that 
+                     these observations and predictions can be applied to other countries with similar banking systems and population demographics."),
+                   p("Below, you can find brief information about the tabs and their purposes."),
+                   p(strong(em("The About page:")),br()," This page provides a brief description of the app's purpose, some information 
+                   about the data, and links to the dataset and additional information. It also includes 
+                   guidance on the use of each tab in the app."),
+                   p(strong(em("The Data page:")),br()," This page allows users to browse and filter the data using the dropdown menus. 
+                     Users can choose to display a specific number of records and even download a CSV file 
+                     of the filtered data for further analysis. The page is designed to make it easy for users 
+                     to explore and understand the data in the app."),
+                   p(strong(em("The Data Exploration:")),br()," This page offers an in-depth analysis of the bank data, allowing users to 
+                     create numerical and graphical summaries, change the type of plots displayed, customize the 
+                     variables used for the summaries, and modify the data in the plots. This page provides users 
+                     with a powerful tool for understanding and interpreting the data, enabling them to gain valuable 
+                     insights and make informed decisions"),
+                   p(strong(em("The Modeling page:")),br()," This offers a range of tools for fitting supervised learning models, including binary 
+                     logistic regression, classification tree, and random forest tree. The page includes three additional 
+                     tabs that provide detailed information about the fitted models, allow users to choose the variables 
+                     used for model fitting and training, and display model training accuracy, test accuracy, and summary 
+                     statistics. The Prediction tab enables users to select the values of the predictors and obtain a prediction 
+                     for the response, providing a powerful tool for data analysis and decision making."),
+                   br(),
+                   br(),
+                   br()
+                   )
+        ),
+      tabPanel( "About the dataset", 
+                tags$style(HTML(
+                  "#my-box1 {
+                  font-family: 'Times New Roman';
+                        font-size: 16px;}"
+                )),
+                box(title = "",
+                    id = "my-box1",
+                    width = 100,
+                    height = 300,
+                    p("The data used in this app pertains to direct marketing campaigns conducted by a Portuguese banking institution. The campaigns 
+                      were conducted primarily through phone calls, and in some cases, multiple contacts were made to a single client in order to determine 
+                      their interest in subscribing to a bank term deposit product. The data includes information on the clients' responses to the marketing 
+                      efforts, as well as other relevant details such as their age, job, and educational background."),
+                    p("The classification goal is to predict if the client will subscribe (yes/no) a ",strong(em("term deposit")),"."),
+                    p("For more information on the dataset, please visit the ", a("website", href="https://archive.ics.uci.edu/ml/datasets/Bank+Marketing#"),
+                      "." ),
+                    p(strong(em("Source: ")), br(), "[Moro et al., 2014] S. Moro, P. Cortez and P. Rita. A Data-Driven Approach to Predict the Success of Bank Telemarketing. 
+                      Decision Support Systems, Elsevier, 62:22-31, June 2014")
+                  )
+              )
+      ),
 
     tags$br()
 #     
@@ -342,20 +414,131 @@ ui <- navbarPage(
 #         tags$br()
 #       )
 #     )
-  )#,
+  ),
 #   
 #   
 #   
-#   ################################################
-#   #### Panel: Documentation                   ####
-#   ################################################
-#   
-#   tabPanel("Documentation",
-#            fluidPage(htmlOutput("doc"))),
-#   
-#   ################################################
-#   #### Panel: About                           ####
-#   ################################################
-#   tabPanel("About",
-#            fluidPage(htmlOutput("abo")))
- )
+  ################################################
+  #### Panel: Data                             ####
+  ################################################
+
+  tabPanel("Data",
+           sidebarLayout(
+             sidebarPanel(
+               checkboxGroupInput("selectVariables","Select variables for Filtering:",
+                                  choices = colNameList,
+                                  selected = colNameList),
+               numericInput("numberOfRows","Select the number of rows to filter: (minimum value is 1)",
+                            value =  10, min = 1, max = 32950),
+               downloadButton('download',"Download the data (CSV)")
+               ),
+             mainPanel(
+               h3("Dataset"),
+               dataTableOutput("table")
+             )
+             )
+           ),
+
+#  
+  ################################################
+  #### Panel: Data Exploration                ####
+  ################################################
+  tabPanel("Data Exploration ",
+           tabsetPanel(
+             type = "tabs",
+             tabPanel( "Plots",
+                      tags$br(),
+                       fluidRow(
+                         column(
+                           4,
+                           radioButtons("barPlotRadio","Select the variable for Bar plot:",
+                                        choices = c("Job Type" = "job",
+                                                    "Marital Status" = "marital",
+                                                    "Education Type" = "education",
+                                                    "Contact type" = "contact"),
+                                        selected = "job"),
+                           numericInput("twistAngle","Select the angle of text on the x-axis:",
+                                        value = 90, min = 18, max = 90)
+                         ),
+                         column(
+                           8, h3("Bar plot", align = "center"),
+                           plotOutput("barPlot")
+                         )
+                       ),
+                       tags$br(),
+                       fluidRow(
+                         column(
+                           4,
+                           radioButtons("histPlotRadio","Select the variable for Histogram plot:",
+                                        choices = c("Age" = "age",
+                                                    "Duration" = "duration",
+                                                    "Campaign" = "campaign"),
+                                        selected = "age"),
+                           numericInput("binWidth","Select the binwidth:",
+                                        value = 2, min = 1, max = 30)
+                         ),
+                         column(
+                           8,h3("Histogram plot", align = "center"),
+                           plotOutput("histPlot")
+                           
+                         )
+                       )),
+             tabPanel("Summaries",
+                      tags$br(),
+                      fluidRow(
+                        column(
+                          4,
+                          checkboxGroupInput("selectVariablesSumm","Select variables for numerical summaries:",
+                                             choices = c("Age" = "age",
+                                                         "Duration" = "duration",
+                                                         "Campaign" = "campaign",
+                                                         "Last contacted from a previous campaign" = "pdays",
+                                                         "contacts performed before this campaign" = "previous"),
+                                             selected =  c("age",
+                                                           "duration",
+                                                           "campaign",
+                                                           "pdays",
+                                                           "previous")),
+                          numericInput("roundValSumm","Round the mean value by:", 2, min = 1, max = 10),
+                          downloadButton('downloadSumm',"Download the data (CSV)")
+                        ),
+                        column(
+                          8,h3("Numerical Summaries", align = "center"),
+                          dataTableOutput("summTable")
+                          )
+                      ),
+                      tags$br(),
+                      fluidRow(
+                        column(
+                          4,
+                          radioButtons("contingencyRadio","Select the variable for contigency table:",
+                                       choices = c("Marital Status" = "marital",
+                                                   "Type of Education" = "education",
+                                                   "Type of Contact" = "contact"),
+                                       selected = "marital"),
+                        ),
+                        column(
+                          8,h3("Contigency table with term deposit", align = "center"),
+                          dataTableOutput("contTable")
+                          
+                        )
+                      )
+                    )
+             )
+           ),
+
+  ################################################
+  #### Panel: Modeling                        ####
+  ################################################
+  tabPanel("Modeling ",
+           tabsetPanel(
+                   type = "tabs",
+                   tabPanel(
+                     "Modeling Info"),
+                   tabPanel(
+                     "Model Fitting"),
+                   tabPanel(
+                     "Prediction")
+           )
+          )
+)
